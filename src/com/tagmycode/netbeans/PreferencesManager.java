32432d6080bad026f31eb5c3b01bdc0b0fad5192
Massimo <massimo.zappino@gmail.com>
2016-01-06 01:25:53 +0100

    Integrated latest version of Plugin Framework

--- src/com/tagmycode/netbeans/PreferencesManager.java
+++ src/com/tagmycode/netbeans/PreferencesManager.java
@@ -1,22 +1,22 @@
 package com.tagmycode.netbeans;
 
-import com.tagmycode.plugin.AbstractStorage;
+import com.tagmycode.plugin.IStorage;
 import org.openide.util.NbPreferences;
 
-class PreferencesManager extends AbstractStorage {
+class PreferencesManager implements IStorage {
 
     @Override
-    protected String read(String string) {
+    public String read(String string) {
         return NbPreferences.forModule(PreferencesManager.class).get(string, "");
     }
 
     @Override
-    protected void write(String key, String value) {
+    public void write(String key, String value) {
         NbPreferences.forModule(PreferencesManager.class).put(key, value);
     }
 
     @Override
-    protected void unset(String string) {
+    public void unset(String string) {
         NbPreferences.forModule(PreferencesManager.class).remove(string);
     }
 
