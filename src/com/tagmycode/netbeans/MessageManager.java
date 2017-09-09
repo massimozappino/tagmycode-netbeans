a06405ee705e072b9ed6e0bbff725b2e7411514b
Massimo Zappino <massimo.zappino@gmail.com>
2017-09-09 14:28:23 +0200

    MessageManager with error dialog

--- src/com/tagmycode/netbeans/MessageManager.java
+++ src/com/tagmycode/netbeans/MessageManager.java
@@ -1,13 +1,21 @@
 package com.tagmycode.netbeans;
 
 import com.tagmycode.plugin.IMessageManager;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.awt.NotificationDisplayer;
 
 class MessageManager implements IMessageManager {
 
     @Override
-    public void error(final String message) {
+    public void errorLog(String message) {
         NotificationDisplayer.getDefault().notify("TagMyCode", NotificationIcons.ERROR, message, null);
+    }
+
+    @Override
+    public void errorDialog(String message) {
+        NotifyDescriptor.Message d = new NotifyDescriptor.Message(message, NotifyDescriptor.ERROR_MESSAGE);
+        DialogDisplayer.getDefault().notify(d);
     }
 
 }
